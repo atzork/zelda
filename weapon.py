2add0ce3371d0927b2b1517145a202ed6875629f
spanasovskiy <atzork@gmail.com>
2022-03-14 02:40:53 +0200

    create repo

---
+++ weapon.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Group
+
+from player import Player
+
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self, player: Player, groups: Group):
+        super().__init__(groups)
+        direction = player.status.split('_')[0]
+
+        # graphic
+        full_path = f'./graphics/weapons/{player.weapon}/{direction}.png'
+        self.image = pygame.image.load(full_path)
+
+        # placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft=player.rect.midright + pygame.math.Vector2(0, 16))
+        elif direction == 'left':
+            self.rect = self.image.get_rect(midright=player.rect.midleft + pygame.math.Vector2(0, 16))
+        elif direction == 'up':
+            self.rect = self.image.get_rect(midbottom=player.rect.midtop - pygame.math.Vector2(12, 0))
+        elif direction == 'down':
+            self.rect = self.image.get_rect(midtop=player.rect.midbottom - pygame.math.Vector2(12, 0))
+        else:
+            self.rect = self.image.get_rect(center=player.rect.center)
