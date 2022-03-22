dce8b9605d7a916358f3ae7c1b9e914064e0105c
zork <atzork@gmail.com>
2022-03-22 23:18:34 +0200

    player damage particle effect class grass damage particles

--- level.py
+++ level.py
@@ -4,6 +4,7 @@ import pygame
 from random import choice
 
 from enemy import Enemy
+from particle import AnimationPlayer
 from settings import *
 from support import import_csv_layout, import_folder
 from tile import Tile
@@ -35,6 +36,9 @@ class Level:
 
         # user interface
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer()
 
     def create_map(self):
         layouts = {
@@ -115,6 +119,10 @@ class Level:
                 if collision_sprites:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
+                            pos = target_sprite.rect.center
+                            offset = pygame.math.Vector2(0, 75)
+                            for leaf in range(random.randint(3, 6)):
+                                self.animation_player.create_grass_particles(pos - offset, self.visible_sprites)
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player, attack_sprite.sprite_type)
@@ -126,7 +134,6 @@ class Level:
             self.player.hurt_time = pygame.time.get_ticks()
 
             # spawn particles
-
 
     def run(self):
         # update
